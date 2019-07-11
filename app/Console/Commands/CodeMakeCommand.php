2df9de452ed9cf85704ccc318aece9b600cc5576
sskycn <sskycn@gmail.com>
2019-07-11 14:50:47 +0800

    clean code

--- app/Console/Commands/CodeMakeCommand.php
+++ app/Console/Commands/CodeMakeCommand.php
@@ -37,6 +37,27 @@ class CodeMakeCommand extends Command
      */
     protected $description = 'Scaffold code for you base on models';
 
+    protected $compileFiles = [
+        //views
+        'resources/views/stubs/model.stub' => 'resources/views/#snakeStudlyName#.blade.php',
+        'resources/views/stubs/show.stub' => 'resources/views/#snakeStudlyName#/show.blade.php',
+        'resources/views/stubs/create.stub' => 'resources/views/#snakeStudlyName#/create.blade.php',
+        'resources/views/stubs/edit.stub' => 'resources/views/#snakeStudlyName#/edit.blade.php',
+        //JS
+        'resources/js/stubs/ssky/list.stub' => 'resources/js/ssky/#snakeStudlyName#/list.js',
+        'resources/js/stubs/ssky/create.stub' => 'resources/js/ssky/#snakeStudlyName#/create.js',
+        'resources/js/stubs/ssky/edit.stub' => 'resources/js/ssky/#snakeStudlyName#/edit.js',
+        'resources/js/stubs/components/list.stub' => 'resources/js/components/#snakeStudlyName#/list.js',
+        'resources/js/stubs/components/create.stub' => 'resources/js/components/#snakeStudlyName#/create.js',
+        'resources/js/stubs/components/edit.stub' => 'resources/js/components/#snakeStudlyName#/edit.js',
+    ];
+
+    protected $compileDetailFiles = [
+        //views
+        'resources/views/stubs/create-form.stub' => 'resources/views/#snakeStudlyName#/create-form.blade.php',
+        'resources/views/stubs/edit-form.stub' => 'resources/views/#snakeStudlyName#/edit-form.blade.php',
+    ];
+
     /**
      * Create a new controller creator command instance.
      *
@@ -61,8 +82,6 @@ class CodeMakeCommand extends Command
         $modelName = $this->argument('model-name');
         $this->modelPath = $this->option('model-path');
         $this->modelNamespace = $this->option('model-namespace');
-
-        $this->createDirectories();
 
         if(empty($modelName)) {
             $this->generateCodes();
@@ -106,79 +125,199 @@ class CodeMakeCommand extends Command
             $tableName = $obj->getTable();
             $columns = Schema::getColumnListing($tableName);
 
-            $this->createView($columns);
+            $this->generateFile($columns);
 
         } else {
             $this->error("Model not exists: " . $class);
         }
     }
 
-    protected function createView($columns) {
+    protected function generateFile($columns) {
 
-        if (! is_dir($directory = $this->getViewPath($this->snakeStudlyName))) {
-            mkdir($directory, 0755, true);
+        if($this->option('check')) {
+            $this->info("will create:");
+            foreach($this->compileFiles as $src=>$target) {
+                $target = $this->replaceModelName($target);
+                $this->info("$target");
+            }
+
+            foreach($this->compileDetailFiles as $src=>$target) {
+                $target = $this->replaceModelName($target);
+                $this->info("$target");
+            }
+
+            return "";
         }
 
-        $modelViewPath = $this->getViewPath($this->snakeStudlyName . ".blade.php");
-        $createViewPath = $this->getViewPath($this->snakeStudlyName . "/create.blade.php");
-        $editViewPath = $this->getViewPath($this->snakeStudlyName . "/edit.blade.php");
-        $showViewPath = $this->getViewPath($this->snakeStudlyName . "/show.blade.php");
-        $createFormViewPath = $this->getViewPath($this->snakeStudlyName . "/create-form.blade.php");
-        $editFormViewPath = $this->getViewPath($this->snakeStudlyName . "/edit-form.blade.php");
+        if($this->option('force')) {
+            foreach($this->compileFiles as $src=>$target) {
+                $content = $this->compileFile($src, $columns);
+                $this->save($target, $content);
+            }
 
-        $modelView = $this->buildView('model', $columns);
-        $createView = $this->buildView('create', $columns);
-        $editView = $this->buildView('edit', $columns);
-        $showView = $this->buildView('show', $columns);
-        $createFormView = $this->buildDetailView('create-form', $columns);
-        $editFormView = $this->buildDetailView('edit-form', $columns);
+            foreach($this->compileDetailFiles as $src=>$target) {
+                $content = $this->compileDetailFile($src, $columns);
+                $this->save($target, $content);
+            }
+        } else {
+            foreach($this->compileFiles as $src=>$target) {
+                if(!file_exists($target)) {
+                    $content = $this->compileFile($src, $columns);
+                    $this->save($target, $content);
+                }
+            }
+
+            foreach($this->compileDetailFiles as $src=>$target) {
+                if(!file_exists($target)) {
+                    $content = $this->compileDetailFile($src, $columns);
+                    $this->save($target, $content);
+                }
+            }
+        }
+        
+        $this->updateComponentBootstrap([
+            "require('./$this->snakeStudlyName/list');",
+            "require('./$this->snakeStudlyName/create');",
+            "require('./$this->snakeStudlyName/edit');",
+        ]);
+    }
+
+    // protected function createView($columns) {
+
+    //     if (! is_dir($directory = $this->getViewPath($this->snakeStudlyName))) {
+    //         mkdir($directory, 0755, true);
+    //     }
+
+    //     $modelViewPath = $this->getViewPath($this->snakeStudlyName . ".blade.php");
+    //     $createViewPath = $this->getViewPath($this->snakeStudlyName . "/create.blade.php");
+    //     $editViewPath = $this->getViewPath($this->snakeStudlyName . "/edit.blade.php");
+    //     $showViewPath = $this->getViewPath($this->snakeStudlyName . "/show.blade.php");
+    //     $createFormViewPath = $this->getViewPath($this->snakeStudlyName . "/create-form.blade.php");
+    //     $editFormViewPath = $this->getViewPath($this->snakeStudlyName . "/edit-form.blade.php");
+
+    //     $modelView = $this->buildView('model', $columns);
+    //     $createView = $this->buildView('create', $columns);
+    //     $editView = $this->buildView('edit', $columns);
+    //     $showView = $this->buildView('show', $columns);
+    //     $createFormView = $this->buildDetailView('create-form', $columns);
+    //     $editFormView = $this->buildDetailView('edit-form', $columns);
 
         
-        $this->save($modelViewPath, $modelView);
-        $this->save($createViewPath, $createView);
-        $this->save($editViewPath, $editView);
-        $this->save($showViewPath, $showView);
-        $this->save($createFormViewPath, $createFormView);
-        $this->save($editFormViewPath, $editFormView);
-    }
+    //     $this->save($modelViewPath, $modelView);
+    //     $this->save($createViewPath, $createView);
+    //     $this->save($editViewPath, $editView);
+    //     $this->save($showViewPath, $showView);
+    //     $this->save($createFormViewPath, $createFormView);
+    //     $this->save($editFormViewPath, $editFormView);
+    // }
+
+    // protected function createJs($columns) {
+    //     if (! is_dir($directory = $this->getJsPath('ssky/' . $this->snakeStudlyName))) {
+    //         mkdir($directory, 0755, true);
+    //     }
+
+    //     $listJsPath = $this->getJsPath('ssky/' . $this->snakeStudlyName . "/list.js");
+    //     $createJsPath = $this->getJsPath('ssky/' . $this->snakeStudlyName . "/create.js");
+    //     $editJsPath = $this->getJsPath('ssky/' . $this->snakeStudlyName . "/edit.js");
+
+    //     $listJs = $this->buildJs('list', $columns);
+    //     $createJs = $this->buildJs('create', $columns);
+    //     $editJs = $this->buildJs('edit', $columns);
+
+    //     $this->save($listJsPath, $listJs);
+    //     $this->save($createJsPath, $createJs);
+    //     $this->save($editJsPath, $editJs);
+
+    //     if (! is_dir($directory = $this->getJsPath('components/' . $this->snakeStudlyName))) {
+    //         mkdir($directory, 0755, true);
+    //     }
+
+    //     $listJsPath = $this->getJsPath('components/' . $this->snakeStudlyName . "/list.js");
+    //     $createJsPath = $this->getJsPath('components/' . $this->snakeStudlyName . "/create.js");
+    //     $editJsPath = $this->getJsPath('components/' . $this->snakeStudlyName . "/edit.js");
+
+    //     $listJs = $this->buildComponent('list', $columns);
+    //     $createJs = $this->buildComponent('create', $columns);
+    //     $editJs = $this->buildComponent('edit', $columns);
+
+    //     $this->save($listJsPath, $listJs);
+    //     $this->save($createJsPath, $createJs);
+    //     $this->save($editJsPath, $editJs);
+
+    //     $this->updateComponentBootstrap([
+    //         "require('./$this->snakeStudlyName/list');",
+    //         "require('./$this->snakeStudlyName/create');",
+    //         "require('./$this->snakeStudlyName/edit');",
+    //     ]);
+    // }
 
     protected function save($path, $content)
     {
         if(empty($content)) return;
 
-        if($this->option('check')) {
-            $this->info("create: " . substr($path, strlen(base_path())));
-            return "";
+        if (! is_dir($directory = dirname($path))) {
+            mkdir($directory, 0755, true);
         }
 
-        if($this->option('force')) {
-            $this->file->put($path, $content);
-        } else {
-            if(!file_exists($path)) {
-                $this->file->put($path, $content);
-            }
-        }
+        $this->file->put($path, $content);
     }
 
-    protected function getViewStub($viewStubName)
-    {
-        return $this->getViewPath("stubs/$viewStubName");
-    }
+    // protected function getViewStub($viewStubName)
+    // {
+    //     return $this->getViewPath("stubs/$viewStubName");
+    // }
 
-    protected function buildView($name, $columns)
-    {
-        $path = $this->getViewStub("$name.stub");
+    // protected function getJsStub($viewStubName)
+    // {
+    //     return $this->getJsPath("stubs/$viewStubName");
+    // }
 
+    // protected function buildView($name, $columns)
+    // {
+    //     $path = $this->getViewStub("$name.stub");
+
+    //     return $this->buildFile($name, $path, $columns);
+    // }
+
+    // protected function buildDetailView($name, $columns) {
+    //     $path = $this->getViewStub("$name.stub");
+    //     return $this->buildDetailFile($name, $path, $columns);
+    // }
+
+    // protected function buildJs($name, $columns)
+    // {
+    //     $path = $this->getJsStub("$name.stub");
+    //     return $this->buildFile($name, $path, $columns);
+    // }
+
+    // protected function buildDetailJs($name, $columns) {
+    //     $path = $this->getJsStub("$name.stub");
+    //     return $this->buildDetailFile($name, $path, $columns);
+    // }
+
+    // protected function buildComponent($name, $columns)
+    // {
+    //     $path = $this->getJsStub("component.stub");
+    //     return $this->buildFile($name, $path, $columns);
+    // }
+
+    protected function compileFile($path, $columns)
+    {
         if(!file_exists($path)) {
             $this->error("File Not Exists: " . substr($path, strlen(base_path())));
             return "";
         }
 
+        $name = basename($path, '.stub');
+
+        $this->info($name);
+        return;
+
         $stub = $this->file->get($path);
 
         $stub = $this->replaceModelName($stub);
 
-        $subStubs = collect(\File::allFiles($this->getViewPath('stubs/')));
+        $subStubs = collect(\File::allFiles(dirname($path)));
 
         foreach($subStubs as $subStub){
             $rel = $subStub->getRelativePathName();
@@ -186,7 +325,7 @@ class CodeMakeCommand extends Command
             if(Str::startsWith($rel, "$name.") && $rel !== "$name.stub") {
                 $subStubName = rtrim($rel, '.stub');
 
-                $line = $this->buildDetailView($subStubName, $columns);
+                $line = $this->compileDetailFile($subStub, $columns);
 
                 $stub = str_replace(
                     ["#$subStubName#"],
@@ -199,8 +338,7 @@ class CodeMakeCommand extends Command
         return $stub;
     }
 
-    protected function buildDetailView($name, $columns) {
-        $path = $this->getViewStub("$name.stub");
+    protected function compileDetailFile($path, $columns) {
 
         if(!file_exists($path)) {
             $this->error("File Not Exists: " . substr($path, strlen(base_path())));
@@ -221,42 +359,38 @@ class CodeMakeCommand extends Command
             ). "\n";
         }
 
-        $outLine = rtrim($outLine, " \n");
+        $outLine = trim($outLine, " \n");
 
         return $outLine;
     }
-    
-    protected function createDirectories()
+
+    protected function updateComponentBootstrap($lines)
     {
-        if (! is_dir($directory = $this->getViewPath('layouts'))) {
-            mkdir($directory, 0755, true);
+        $path = $this->getJsPath('components/bootstrap.js');
+
+        if(!file_exists($path)) {
+            $this->error("File Not Exists: " . substr($path, strlen(base_path())));
+            return "";
         }
 
-        if (! is_dir($directory = $this->getJsPath('components'))) {
-            mkdir($directory, 0755, true);
+        if($this->option('check')) {
+            $this->info("update: " . substr($path, strlen(base_path())));
+            return "";
         }
 
-        if (! is_dir($directory = $this->getJsPath('ssky'))) {
-            mkdir($directory, 0755, true);
+        $content = $this->file->get($path);
+
+        if(!Str::endsWith($content, "\n")) {
+            $this->file->append($path, "\n");
         }
 
-        if (! is_dir($directory = $this->getSassPath(''))) {
-            mkdir($directory, 0755, true);
-        }
-
-        if (! is_dir($directory = $this->getControllerPath(''))) {
-            mkdir($directory, 0755, true);
-        }
-
-        if (! is_dir($directory = $this->getContractPath(''))) {
-            mkdir($directory, 0755, true);
-        }
-
-        if (! is_dir($directory = $this->getRepositoryPath(''))) {
-            mkdir($directory, 0755, true);
+        foreach($lines as $line) {
+            if(!Str::contains($content, $line)) {
+                $this->file->append($path, "$line\n");
+            }
         }
     }
-
+    
     protected function getViewPath($path)
     {
         return implode(DIRECTORY_SEPARATOR, [
--- resources/js/components/bootstrap.js
+++ resources/js/components/bootstrap.js
@@ -3,3 +3,6 @@ require('./auth/register');
 require('./user/list');
 require('./user/create');
 require('./user/edit');
+require('./role/list');
+require('./role/create');
+require('./role/edit');
