e891647eb3f1bd6d8e7df9df337bae19f4e2886f
sskycn <sskycn@gmail.com>
2019-08-05 12:38:37 +0800

    remove code of role

--- resources/js/components/bootstrap.js
+++ resources/js/components/bootstrap.js
@@ -1,8 +1,5 @@
 require('./auth/login');
 require('./auth/register');
-require('./role/list');
-require('./role/create');
-require('./role/edit');
 require('./user/list');
 require('./user/create');
 require('./user/edit');
--- resources/js/components/role/create.js
+++
@@ -1,5 +0,0 @@
-var base = require('role/create');
-
-Vue.component('ssky-role-create', {
-    mixins: [base]
-});
