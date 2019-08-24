c18e66d30fa61ef5f1d8652887e997f642f26249
sskycn <sskycn@gmail.com>
2019-08-24 22:47:10 +0800

    clean code

--- resources/views/user/create-form.blade.php
+++ resources/views/user/create-form.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group row">
     <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('First Name') }}</label>
     <div class="col-md-6">
-        <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name"
-            value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
+        <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror"
+            name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
         @error('first_name')
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
