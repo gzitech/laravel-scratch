7f770656270f70768a6dd74933d41b56105986cd
sskycn <sskycn@gmail.com>
2019-08-26 17:28:06 +0800

    clean code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,7 +10,6 @@
         type='text/css'>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-
 <body>
     <div id="ssky-app">
         @none
