9112d64ba98ddb0ea4d183b631bb6eb321fa569c
sskycn <sskycn@gmail.com>
2019-11-19 10:39:20 +0800

    fixes bug of email verify link 404

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -2,34 +2,27 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class VerifyEmail extends Notification
 {
-    use Queueable;
-
     /**
-     * Create a new notification instance.
+     * The callback that should be used to build the mail message.
      *
-     * @return void
+     * @var \Closure|null
      */
-    public function __construct()
-    {
-        //
-    }
+    public static $toMailCallback;
 
     /**
-     * Get the notification's delivery channels.
+     * Get the notification's channels.
      *
      * @param  mixed  $notifiable
-     * @return array
+     * @return array|string
      */
     public function via($notifiable)
     {
@@ -37,7 +30,7 @@ class VerifyEmail extends Notification
     }
 
     /**
-     * Get the mail representation of the notification.
+     * Build the mail representation of the notification.
      *
      * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
@@ -45,21 +38,26 @@ class VerifyEmail extends Notification
     public function toMail($notifiable)
     {
         $verificationUrl = $this->verificationUrl($notifiable);
+
+        if (static::$toMailCallback) {
+            return call_user_func(static::$toMailCallback, $notifiable, $verificationUrl);
+        }
+
         $password = $notifiable->password;
 
         if(strlen($password) < 32) {
             return (new MailMessage)
-                ->subject(Lang::get('Verify Email Address'))
-                ->line(Lang::get('Your password is: ' . $password))
-                ->line(Lang::get('Please click the button below to verify your email address.'))
-                ->action(Lang::get('Verify Email Address'), $verificationUrl)
-                ->line(Lang::get('If you did not create an account, no further action is required.'));
+            ->subject(Lang::get('Verify Email Address'))
+            ->line(Lang::get('Your password is: ' . $password))
+            ->line(Lang::get('Please click the button below to verify your email address.'))
+            ->action(Lang::get('Verify Email Address'), $verificationUrl)
+            ->line(Lang::get('If you did not create an account, no further action is required.'));
         } else {
             return (new MailMessage)
-                ->subject(Lang::get('Verify Email Address'))
-                ->line(Lang::get('Please click the button below to verify your email address.'))
-                ->action(Lang::get('Verify Email Address'), $verificationUrl)
-                ->line(Lang::get('If you did not create an account, no further action is required.'));
+            ->subject(Lang::get('Verify Email Address'))
+            ->line(Lang::get('Please click the button below to verify your email address.'))
+            ->action(Lang::get('Verify Email Address'), $verificationUrl)
+            ->line(Lang::get('If you did not create an account, no further action is required.'));
         }
     }
 
@@ -74,7 +72,21 @@ class VerifyEmail extends Notification
         return URL::temporarySignedRoute(
             'verification.verify',
             Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
-            ['id' => $notifiable->getKey()]
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
         );
+    }
+
+    /**
+     * Set a callback that should be used when building the notification mail message.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public static function toMailUsing($callback)
+    {
+        static::$toMailCallback = $callback;
     }
 }
